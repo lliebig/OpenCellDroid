3751582a0a4de4c7f6b7b8af72b4d49c84071955
Leo Liebig <info@leoliebig.de>
2013-05-07 16:52:50 +0200

    Layout update for MainActivity

--- src/de/opencelldroid/MainActivity.java
+++ src/de/opencelldroid/MainActivity.java
@@ -3,9 +3,11 @@ package de.opencelldroid;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import de.opencelldroid.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		int viewId = v.getId();
+		
+		switch(viewId){
+		case R.id.btn_main_submit_cell:
+			//do something
+			break;
+		case R.id.btn_main_show_cells:
+			//do something else
+			break;
+		}
+		
 	}
 
 }
