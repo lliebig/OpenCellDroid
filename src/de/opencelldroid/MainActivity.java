e8d1f19d7d7e7ebf3771a6cad0eb8d19f5f0b573
Leo Liebig <info@leoliebig.de>
2013-05-28 16:56:17 +0200

    Fix

--- src/de/opencelldroid/MainActivity.java
+++ src/de/opencelldroid/MainActivity.java
@@ -8,21 +8,46 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import de.opencelldroid.loc.Cell;
 import de.opencelldroid.loc.LocationHelper;
 
-public class MainActivity extends Activity implements OnClickListener{
-	
+public class MainActivity extends Activity implements OnClickListener {
+
 	private static final String TAG = "OpenCellDroid";
-	
+
 	private GpsListener mGpsListener;
 	private Location mCurrentLocation;
+
+	static final LatLng BERLIN = new LatLng(52.519171, 13.406091);
+	private GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		mGpsListener = new GpsListener();
+
+		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		moveMapTo(BERLIN, "Berlin", "Default position");
+	}
+
+	private void moveMapTo(LatLng position, String title, String snippet) {
+		Marker m = map.addMarker(new MarkerOptions().position(position));
+		m.setTitle(title);
+		m.setSnippet(snippet);
+		m.showInfoWindow();
+		
+		// move the camera to the new position with a zoom of 15
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15));
 	}
 
 	@Override
@@ -35,22 +60,24 @@ public class MainActivity extends Activity implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		int viewId = v.getId();
-		
-		switch(viewId){
+
+		switch (viewId) {
 		case R.id.btn_main_submit_cell:
 			try {
 				Cell cell = LocationHelper.getCurrentCell(this);
 				Log.d(TAG, cell.toString());
+				Toast.makeText(this, "You are connected with:\n" + cell.toString(), Toast.LENGTH_LONG).show();
+				
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} 
-			
+			}
+
 			break;
 		case R.id.btn_main_show_cells:
-			
+
 			try {
-				//FIXME: Timeout???
+				// FIXME: Timeout???
 				LocationHelper.requestGpsLocation(this, mGpsListener);
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
@@ -58,29 +85,31 @@ public class MainActivity extends Activity implements OnClickListener{
 			}
 			break;
 		}
-		
+
 	}
 
-	//inner classes
+	// inner classes
 	class GpsListener implements LocationListener {
 
 		@Override
 		public void onLocationChanged(Location location) {
 			mCurrentLocation = location;
 			Log.d(TAG, mCurrentLocation.toString());
+			LatLng curPos = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+			moveMapTo(curPos, "Your position", "You are here!");
 		}
-	
+
 		@Override
 		public void onProviderDisabled(String provider) {
 		}
-	
+
 		@Override
 		public void onProviderEnabled(String provider) {
 		}
-	
+
 		@Override
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 		}
-		
+
 	}
 }
